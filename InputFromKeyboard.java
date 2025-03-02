8af1b25ab75686c862b1d1a42c3b24c866eca902
Vanh <vanhtran18.soict@gmail.com>
2025-03-02 21:40:26 +0700

    Complete Exercise 6.3

--- InputFromKeyboard.java
+++ InputFromKeyboard.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class InputFromKeyboard {
-	public static void main(String args[]){
+	public static void main(String args[]) {
 		Scanner keyboard = new Scanner(System.in);
 		
 		System.out.println("What's your name?");
@@ -13,5 +13,6 @@ public class InputFromKeyboard {
 		
 		System.out.println("Mrs/Ms. " + strName + ", " + iAge + " years old. "
 				+ "Your height is " + dHeight + ".");
+		keyboard.close();
 	}
 }
